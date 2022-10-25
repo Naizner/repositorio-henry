be67bc6d56ed052632d837f6cd8e58079585a053
Naizner <116586383+Naizner@users.noreply.github.com>
2022-10-24 23:26:55 -0300

    Clases en JS intento de creacion

---
+++ archivo-ejemplo.js
@@ -0,0 +1,13 @@
+class Alumno {
+    constructor(nombre, apellido){
+        this._nombre=nombre;
+        this._apellido=apellido;
+    }
+
+    get nombre(){
+        return this._nombre;
+    }
+    set nombre(nombre){
+        this._nombre=nombre;
+    }
+}
